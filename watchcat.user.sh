733fad0dd91af1d21b67a38edb57c1946d26302f
samara1532 <44088858+samara1532@users.noreply.github.com>
2024-12-14 17:03:20 +0300

    watchcat.user.sh

--- watchcat.user.sh
+++ watchcat.user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -O /etc/homeproxy/resources/gfw_list.txt https://raw.githubusercontent.com/itdoginfo/allow-domains/main/Russia/inside-raw.lst
-sleep 60 && /etc/init.d/homeproxy restart &
+sleep 100 && /etc/init.d/homeproxy restart &
